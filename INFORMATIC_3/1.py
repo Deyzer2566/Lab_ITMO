207a0f553563e5744b33cf08a6d4f2e7752872a2
ScaryCrocodile <50776487+Deyzer2566@users.noreply.github.com>
2022-10-19 17:56:38 +0300

    Update 1.py
    решение лабы 2021 -> лаба 2022

--- INFORMATIC_3/1.py
+++ INFORMATIC_3/1.py
@@ -1,28 +1,29 @@
 import random
+import re
 isu = 367298
-eye = [':',';','X','8','=']
+eye = [':',';','X','8','=','[']
 nose = ['-','<','-{','<{']
 mouth = ['(',')','O','|','\\','/','P']
-my_smile = eye[isu%5]+nose[isu%4]+mouth[isu%7]
+my_smile = eye[isu%6]+nose[isu%4]+mouth[isu%7]
 print('мой смайлик:',my_smile)
 def gen():#генерация тестов
     s = []
     count = 0
     for i in range(20):
         if random.randint(0,8)==3:
-            s.extend(my_smile)
+            s.append(my_smile)
             count+=1
-        eye_ind = random.randint(0,4)
+        eye_ind = random.randint(0,5)
         nose_ind=random.randint(0,3)
         mouth_ind=random.randint(0,6)
         smile = eye[eye_ind]+nose[nose_ind]+mouth[mouth_ind]
-        s.extend(smile)
+        s += [smile]
         if(smile == my_smile):
             count += 1
-    return ''.join(s),count
+    return ' '.join(s),count
 test=[]
 for i in range(10):
     test += [gen()]
 print('Вывод программы   Правильный ответ')
 for i in range(10):
-    print('       ',test[i][0].count(my_smile),'              ', test[i][1])
+    print('       ',len(re.findall('X-\{\)',test[i][0])),'              ', test[i][1])
