dc803252e2900e9cc6e22a0d71c55a21d4f4f383
ScaryCrocodile <50776487+Deyzer2566@users.noreply.github.com>
2022-11-06 13:14:15 +0300

    Update 1.py

--- Infortatic/4/1.py
+++ Infortatic/4/1.py
@@ -2,87 +2,6 @@ print((367298+8*2)%36)#6
 #JSON -> XML
 #ВТОРНИК
 
-def parseString(s:str):
-    e=0
-    while True:
-        e = min(s.find(',',e+1),s.find('"'))
-        if e == -1:
-            e = max(s.find(',', e + 1), s.find('"'))
-        if e == -1:
-            return s,s.__len__()
-        in_seq=True
-        for j in range(e+1):
-            in_seq = in_seq ^ (s[j]=='"' and s[j-1]!='\\')
-        if not in_seq:
-            break
-    return s[:e],e+1
-def parseArray(s:str):
-    p=0
-    e=0
-    elements=[]
-    while True:
-        e = min(s.find(',',p+1),s.find(']',p+1))
-        if e == -1:
-            e=max(s.find(',', p+1), s.find(']', p+1))
-        if e == -1:
-            e=s.__len__()
-        if p == e:
-            break
-        if s[p]==']':
-            break
-        if s[p] == '[':
-            data = parseArray(s[p+1:])
-            elements += [data[0]]
-            p += data[1]
-        elif s[p] == '"':
-            data = parseString(s[p+1:])
-            elements += [data[0]]
-            p += data[1]
-        elif s[p] == '{':
-            data = parseObject(s[p+1:])
-            elements += [data[0]]
-            p += data[1]
-        else:
-            mean = s[p:e]
-            elements += [mean]
-            p = e
-        while s[p] == ',':
-            p += 1
-        in_seq=True
-        for j in range(e+1):
-            in_seq = in_seq ^ (s[j]==']')
-        if not in_seq:
-            break
-    return elements,e
-def parseObject(s:str):
-    p=0
-    ret = {}
-    end=s.__len__()
-    while p != end:
-        end = s.find('}',p)
-        name_p1 = s.find('"', p) + 1
-        name_p2 = s.find('"', name_p1 + 1)
-        name = s[name_p1:name_p2]
-        p = name_p2+2
-        if s[p] == '[':
-            data = parseArray(s[p+1:])
-            mean = data[0]
-            p += data[1]
-        elif s[p] == '"':
-            data = parseString(s[p+1:])
-            mean = data[0]
-            p += data[1]
-        elif s[p]=='{':
-            data = parseObject(s[p+1:])
-            mean = data[0]
-            p += data[1]
-        else:
-            mean = s[name_p2+2:s.find(',',name_p2+2)]
-            p += s.find(',',name_p2+2)
-        ex=True
-        ret = dict(list(ret.items())+list({name:mean}.items()))
-    return ret,end
-#print(parseObject("{{\"name1\":[\"lol\"],\"name2\":[]},{}}"))
 def fromArrayToXML(l:list,lvl:int):
     ret=''
     for k,j in enumerate(l):
@@ -108,31 +27,6 @@ def toXML(s:dict,lvl:int):
             ret += value
         ret += '</' + key + '>'+'\n'
     return ret
-# def fromArrayToXML(l:list,lvl:int):
-#     ret=''
-#     for k,j in enumerate(l):
-#         ret += '<'+str(k)+'>'
-#         if type(j) is list:
-#             ret += fromArrayToXML(j,lvl+1)
-#         elif type(j) is dict:
-#             ret += toXML(j,lvl+1)
-#         else:
-#             ret += str(j)
-#         ret += '</'+str(k)+'>'
-#     return ret
-#
-# def toXML(s:dict,lvl:int):
-#     ret=''
-#     for key,value in s.items():
-#         ret += '<'+key+'>'
-#         if type(value) is dict:
-#             ret += toXML(value,lvl+1)
-#         elif type(value) is list:
-#             ret += fromArrayToXML(value,lvl+1)
-#         else:
-#             ret += value
-#         ret += '</' + key + '>'
-#     return ret
 if __name__=='__main__':
     f = open("расписание.json",encoding='utf-8')
     s = f.read()
